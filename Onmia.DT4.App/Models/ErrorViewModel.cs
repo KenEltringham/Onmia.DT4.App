16090bac8a8b4dc17f8dfd66598db9833ed802cf
KenEltringham <ken.eltringham@digital.mod.uk>
2025-05-19 22:05:39 +0100

    Add project files.

---
+++ Onmia.DT4.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Onmia.DT4.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
